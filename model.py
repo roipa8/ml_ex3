d6802d3ffa31738f81a67e77619145de25518c82
ofir.kapustian <ofir.kapustian@servicenow.com>
2022-07-04 23:31:15 +0300

    done

--- model.py
+++ model.py
@@ -1,14 +1,25 @@
-from sklearn.linear_model import LinearRegression
+from xgboost import XGBRegressor
+from sklearn.preprocessing import StandardScaler
 
 
 class Model:
     def __init__(self):
-        self.model = LinearRegression()
+        self.scaler = StandardScaler()
+        self.model = XGBRegressor(
+            base_score=0.5, booster='gbtree', colsample_bylevel=1,
+            colsample_bynode=1, colsample_bytree=0.7, gamma=0.0, gpu_id=-1,
+            importance_type='gain', interaction_constraints='',
+            learning_rate=0.1, max_delta_step=0, max_depth=12,
+            min_child_weight=7, monotone_constraints='()',
+            n_estimators=100, n_jobs=0, num_parallel_tree=1, random_state=42,
+            reg_alpha=0, reg_lambda=1, scale_pos_weight=1, subsample=1,
+            tree_method='exact', validate_parameters=1, verbosity=None)
 
     def fit(self, X, y):
-        self.model = self.model.fit(X, y)
+        X_transformed = self.scaler.fit_transform(X)
+        self.model = self.model.fit(X_transformed, y)
 
     def predict(self, X):
-        return self.model.predict(X)
+        return self.model.predict(self.scaler.transform(X))
 
 
